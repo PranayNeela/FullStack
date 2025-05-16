757b59a7ab50d0042b3d5f6fceebe400acf9a05b
Neela Pranay <neela.10828854@ltimindtree.com>
2025-05-16 17:53:11 +0530

    Initial Commit

---
+++ springapp/src/main/java/com/example/springapp/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.example.springapp.model.User;
+import com.example.springapp.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.status(200).body(userService.getAllUsers());
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    
+   
+}
---
+++ springapp/src/main/java/com/example/springapp/service/UserService.java
@@ -0,0 +1,53 @@
+package com.example.springapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+
+import com.example.springapp.model.User;
+import com.example.springapp.repository.UserRepo;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepo userRepo;
+
+ 
+ 
+    public User saveUser(User user){
+    return userRepo.save(user);
+  }
+
+
+   
+
+    public List<User> getAllUsers(){
+        return userRepo.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepo.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepo.deleteById(id);
+    }
+
+    // public void updateUser(Long id,User user) {
+    //     if(userRepo.existsById(id)) {
+    //         user.setId(id);
+    //         userRepo.save(user);
+    //     }
+    // }
+
+
+
+    
+
+}
